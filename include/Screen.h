2c7b61d2b54300811dadfe79d85489028dc1e87e
qwercik <erykandrzejewski2000@gmail.com>
2016-06-03 23:27:30 +0200

    Added class Screen

---
+++ include/Screen.h
@@ -0,0 +1,62 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <types.h>
+
+class Screen
+{
+
+public:
+
+	enum Color
+	{
+		BLACK,
+		BLUE,
+		GREEN,
+		AQUA,
+		RED,
+		PURPLE,
+		ORANGE,
+		WHITE,
+		GRAY,
+		LIGHT_BLUE,
+		LIGHT_GREEN,
+		CYAN,
+		LIGHT_RED,
+		PINK,
+		YELLOW,
+		BRIGHT_WHITE
+	};
+
+	
+	Screen(Color fontColor = WHITE, Color backgroundColor = BLACK);
+
+	void printChar(uint8_t character);
+	void printString(string stringToPrint);
+
+	void clearScreen();
+	void clearLine(uint8_t lineNumber);
+
+	void setColor(Color fontColor, Color backgroundColor);
+	void setFontColor(Color fontColor);
+	void setBackgroundColor(Color backgroundColor);
+
+	//void moveCursorXY(uint8_t positionX, uint8_t positionY); //not developed yet
+private:
+
+	const uint8_t screenHeight;
+	const uint8_t screenWidth;
+
+	int16_t* videoMemory;
+
+	Color fontColor, backgroundColor;
+
+	uint8_t currentPositionX, currentPositionY;
+	
+	
+	void updateCursor(); //It updates the underline character ('_') 
+	void scrollScreen();
+
+};
+
+#endif
---
+++ kernel/Screen.cpp
@@ -0,0 +1,122 @@
+#include <Screen.h>
+#include <ioports.h>
+
+Screen::Screen(Color fontColor, Color backgroundColor) :screenHeight(25), screenWidth(80)
+{
+	this -> fontColor = fontColor;
+	this -> backgroundColor = backgroundColor;
+
+	videoMemory = (int16_t*) 0xB8000;
+
+	currentPositionX = 0;
+	currentPositionY = 0;
+}
+
+void Screen::printChar(uint8_t character)
+{
+	switch (character)
+	{
+		case '\n':
+			currentPositionX = 0;
+			if (currentPositionY == screenHeight - 1)
+				scrollScreen();
+			else
+				currentPositionY++;
+
+			updateCursor();
+			break;
+
+		case '\r':
+			currentPositionX = 0;
+			updateCursor();
+			break;
+
+
+		default:
+			uint16_t resultCursorPosition = currentPositionY * screenWidth + currentPositionX;
+			uint16_t videoData = ((backgroundColor << 1 | fontColor) << 8) | character;
+
+			*(videoMemory + resultCursorPosition) = videoData;
+
+			if (currentPositionX + 1 == screenWidth)
+			{
+				if (currentPositionY == screenHeight - 1)
+					scrollScreen();
+				currentPositionX = 0;
+			}
+			else
+				currentPositionX++;
+			}
+			updateCursor();
+
+}
+
+void Screen::printString(string stringToPrint)
+{
+	int i = 0;
+	
+	while (stringToPrint[i++] != '\0')
+		printChar(stringToPrint[i - 1]);
+}
+
+void Screen::clearLine(uint8_t lineNumber) //Bug, i fix this later
+{
+
+	currentPositionX = 0;
+	updateCursor();
+
+	uint16_t videoData = ' ' | ((backgroundColor * 17) << 8);
+	uint8_t linePosition = screenWidth * lineNumber;
+
+	for (int i = 0; i < screenWidth; i++)
+		*(videoMemory + linePosition + i) = videoData;
+}
+
+void Screen::clearScreen()
+{
+	currentPositionX = 0;
+	currentPositionY = 0;
+
+	updateCursor();
+
+	uint16_t videoData = 0x20 | ((0x07) << 8); //try later to change this
+
+	for (int i = 0; i < screenHeight * screenWidth; i++)
+		*(videoMemory + i) = videoData;
+}
+
+void Screen::setColor(Color fontColor, Color backgroundColor)
+{
+	this -> fontColor = fontColor;
+	this -> backgroundColor = backgroundColor;
+}
+
+void Screen::setFontColor(Color fontColor)
+{
+	this -> fontColor = fontColor;
+}
+
+void Screen::setBackgroundColor(Color backgroundColor)
+{
+	this -> backgroundColor = backgroundColor;
+}
+
+void Screen::updateCursor()
+{
+	uint16_t cursorPosition = currentPositionY * screenWidth + currentPositionX;
+
+	outportb(0x3D4, 14); //Setting to send high byte of 16bit cursorposition variable
+	outportb(0x3D5, (cursorPosition >> 8) & 0xFF); //Sending high byte
+	outportb(0x3D4, 15); //Setting to send low byte
+	outportb(0x3D5, cursorPosition & 0xFF); //Sending low byte of 16bit cursorposition variable
+}
+
+void Screen::scrollScreen()
+{
+	for (int i = 0; i < (screenHeight - 1) * screenWidth * 2; i++)
+		*(videoMemory + i) = *(videoMemory + i + screenWidth);
+
+	currentPositionY = screenHeight - 1;
+
+	updateCursor();
+}
