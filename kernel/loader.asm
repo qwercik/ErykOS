7877d6f8a2805911652e0797b021e13c27324922
qwercik <erykandrzejewski2000@gmail.com>
2016-06-03 23:20:55 +0200

    Add kernel loader

---
+++ kernel/loader.asm
@@ -0,0 +1,35 @@
+MAGIC 		equ 0x1BADB002
+FLAGS		equ (1<<0 | 1<<1)
+CHECKSUM	equ -(MAGIC + FLAGS)
+
+
+STACK_SIZE	equ 16384 ;16kB
+
+section .multiboot
+align 4
+
+	dd MAGIC
+	dd FLAGS
+	dd CHECKSUM
+
+section .text
+
+global loader
+loader:
+	mov esp, stack + STACK_SIZE
+
+	push eax ;multiboot magic number
+	push ebx ;multiboot info structure
+
+	extern main
+	call main
+
+	cli
+	hlt
+		
+	jmp $
+
+section .bss
+
+stack:
+	resb STACK_SIZE
